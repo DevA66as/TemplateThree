071647bdf44288901ca1c4752b8bd6aad98ebb53
Abbas Mohammed <99032870+DevA66as@users.noreply.github.com>
2022-04-22 07:08:16 +0300

    Add files via upload

---
+++ Gatgo.js
@@ -0,0 +1,38 @@
+let myTimeSec = document.querySelector(".sec");
+console.log(myTimeSec);
+let countDownSec = function () {
+  myTimeSec.innerHTML -= 1;
+  if (myTimeSec.innerHTML === "0") {
+    return (myTimeSec.innerHTML = 60);
+  }
+};
+let counterSec = setInterval(countDownSec, 1000);
+
+let myTimeMin = document.querySelector(".min");
+console.log(myTimeMin);
+let countDownMin  = function () {
+  myTimeMin.innerHTML -= 1;
+  if (myTimeMin.innerHTML === "0") {
+    return (myTimeMin.innerHTML = 60);
+  }
+};
+let counterMin = setInterval(countDownMin, 60000);
+
+let myTimeHours = document.querySelector(".hours");
+console.log(myTimeHours);
+let countDownHours  = function () {
+  myTimeHours.innerHTML -= 1;
+  if (myTimeHours.innerHTML === "0") {
+    return (myTimeHours.innerHTML = 24);
+  }
+};
+let counterHours = setInterval(countDownHours, 3600000);
+let myTimeDays = document.querySelector(".days");
+console.log(myTimeDays);
+let countDownDays  = function () {
+  myTimeDays.innerHTML -= 1;
+  if (myTimeDays.innerHTML === "0") {
+    return (myTimeDays.innerHTML = "Start");
+  }
+};
+let counterDays = setInterval(countDownDays, 86400000);
