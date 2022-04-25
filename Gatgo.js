8d029b6a02979f782e9c02c8e4fba867ed2367db
Abbas Mohammed <99032870+DevA66as@users.noreply.github.com>
2022-04-25 05:23:29 +0300

    Update Gatgo.js

--- Gatgo.js
+++ Gatgo.js
@@ -47,3 +47,48 @@ myBtn.onclick = function () {
     behavior:"smooth"
   })
 }
+//============sideMenu===========
+let myButton = document.querySelector(".menuButton");
+let mySideMenu = document.querySelector(".sideMenu");
+let menuExit = document.querySelector(".exitMenu");
+let myFulMenu = document.querySelector(".fulMenu");
+let allNotEnd = document.querySelectorAll(".sideBar li:not(:last-child)");
+let exitFulMenu = document.querySelector(".exitFullMenu")
+// menu button show and hide
+myButton.addEventListener("click", () => {
+  let myButtonHidding = function () {
+    myButton.style.cssText = "display: none;"
+    mySideMenu.style.cssText = "display: block;"
+  }
+  // small menu exit option 
+  setTimeout(myButtonHidding, 200);
+  menuExit.addEventListener("click", () => {
+    mySideMenu.style.cssText = "display: none;";
+    myButton.style.cssText = "display: block;";
+  });
+})
+// lis which when click it the full menu will apear
+allNotEnd.forEach((el) => {
+  el.addEventListener("click", () => {
+    myFulMenu.style.cssText = "display: flex;";
+    mySideMenu.style.cssText = "display: none;";
+  })
+})
+// full menu exit option
+exitFulMenu.addEventListener("click", () => {
+  myFulMenu.style.cssText = " display: none;"
+  myButton.style.cssText = "display: block;";
+})
+
+// set  color in local storage
+let firtColor = document.querySelectorAll(".colorShadow");
+firtColor.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    window.localStorage.setItem("color", `${ele.dataset.color}`);
+  });
+})
+//=====select root and get new color from local storage
+let root = document.documentElement;
+root.addEventListener("mousemove", (e) => {
+  root.style.setProperty("--main-sec-color", `${window.localStorage.getItem("color")}`);
+});
